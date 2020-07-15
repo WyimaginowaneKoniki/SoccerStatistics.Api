ef91926fa827ea1d04e824a12a975e2c3f1abbec
Bartosz Chodkowski <pieter444@wp.pl>
2020-07-15 16:46:15 +0200

    Add Leagueid Leaguename and matches to DTO

--- SoccerStatistics.Api.Core/AutoMapper/Profiles/AutoMapperRoundProfile.cs
+++ SoccerStatistics.Api.Core/AutoMapper/Profiles/AutoMapperRoundProfile.cs
@@ -8,7 +8,9 @@ namespace SoccerStatistics.Api.Core.AutoMapper.Profiles
     {
         public AutoMapperRoundProfile()
         {
-            CreateMap<Round, RoundDTO>();
+            CreateMap<Round, RoundDTO>()
+                .ForMember(dto => dto.LeagueId, e => e.MapFrom(p => p.League.Id))
+                .ForMember(dto => dto.LeagueName, e => e.MapFrom(p => p.League.Name));
         }
     }
 }
--- SoccerStatistics.Api.Core/DTO/RoundDTO.cs
+++ SoccerStatistics.Api.Core/DTO/RoundDTO.cs
@@ -1,8 +1,16 @@
-﻿namespace SoccerStatistics.Api.Core.DTO
+﻿using System.Collections.Generic;
+
+namespace SoccerStatistics.Api.Core.DTO
 {
     public class RoundDTO
     {
         public uint Id { get; set; }
         public string Name { get; set; }
+        public uint LeagueId { get; set; }
+        public string LeagueName { get; set; }
+
+        public virtual IEnumerable<MatchDTO> Matches { get; set; }
+
+
     }
 }

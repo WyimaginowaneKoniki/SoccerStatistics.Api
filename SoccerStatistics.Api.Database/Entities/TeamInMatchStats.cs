2ac663550d573ff0774c4a15297a576fcf031d40
Dominik Tyc <Domikxf@gmail.com>
2020-08-13 00:58:44 +0200

    Add validation attribute for formation

--- SoccerStatistics.Api.Database/Entities/TeamInMatchStats.cs
+++ SoccerStatistics.Api.Database/Entities/TeamInMatchStats.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SoccerStatistics.Api.Database.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace SoccerStatistics.Api.Database.Entities
 {
@@ -13,6 +14,7 @@ namespace SoccerStatistics.Api.Database.Entities
         public uint BallPossesion { get; set; }
         [Required]
         [StringLength(20)]
+        [Formation]
         public string Formation { get; set; }
         [Required]
         public Team Team { get; set; }

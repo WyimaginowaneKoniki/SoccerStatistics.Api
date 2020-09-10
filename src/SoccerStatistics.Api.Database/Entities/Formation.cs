2eec43f0a1afc8722ddcda0d0b9c78fe2350a4ea
Dominik Tyc <Domikxf@gmail.com>
2020-09-10 19:35:20 +0200

    Use built-in attribute

--- src/SoccerStatistics.Api.Database/Entities/Formation.cs
+++ src/SoccerStatistics.Api.Database/Entities/Formation.cs
@@ -9,12 +9,13 @@ namespace SoccerStatistics.Api.Database.Entities
         [Required]
         public Player Player { get; set; }
         [Required]
-        [PositionInFormation]
+        [Range(0, 10)]
         // Unique player's number in formation
-        /*  e.g. "4-4-2"
-         *     9  10
-         *  5  6  7  8
-         *  1  2  3  4
+        /*  e.g. "4-4-2" where 0 == goalkeeper
+         *    9 10
+         *  5 6 7 8
+         *  1 2 3 4
+         *     0
          */
         public uint PositionNumber { get; set; }
     }
